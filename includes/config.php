2aa1ec3ef7f3f9077c80a686e5a2074db094cc5f
Luis Enrique Leal isla García <kiike76@hotmail.com>
2020-10-27 07:56:54 -0600

    Add files via upload
    Actualización de hace un año, sin modificación previamente.
    
    Se trataría de mejorar en MVC con la arquitectura de software mediante RESTFUL

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| 
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', 'enriquepro123' );             // Set database password
+  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+?>
